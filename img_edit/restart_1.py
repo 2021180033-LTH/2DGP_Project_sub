affece98cd05c9194b1bbf1495c590ccacdd7dcc
2021180033-LTH <mdk73000@naver.com>
2022-11-19 16:03:29 +0900

    restart count added

---
+++ img_edit/restart_1.py
@@ -0,0 +1,69 @@
+from pico2d import *
+import game_framework
+import game_world
+from count import One
+import third_map
+
+one = None
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+            game_framework.quit()
+
+
+
+def enter():
+    global one
+
+    one = One()
+
+    game_world.add_object(one, 0)
+
+
+def exit():
+    game_world.clear()
+
+
+def update():
+    for game_object in game_world.all_objects():
+        game_object.update()
+
+    delay(1)
+    game_world.remove_object(one)
+    game_framework.change_state(third_map)
+
+
+def draw_world():
+    for game_object in game_world.all_objects():
+        game_object.draw()
+
+
+def draw():
+    clear_canvas()
+    draw_world()
+    update_canvas()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def test_self():
+    import sys
+    this_module = sys.modules['__main__']
+    pico2d.open_canvas()
+    game_framework.run(this_module)
+    pico2d.close_canvas()
+
+
+if __name__ == '__main__':
+    test_self()
